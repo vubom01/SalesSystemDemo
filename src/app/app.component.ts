a7f800ceac07911f57d1b99ea0e5af9eb3a81d74
vubom01 <lehuyvu1852001@gmail.com>
2021-03-07 21:50:01 +0700

    test material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
+import { CommonService } from './Services/common.service';
+import { ServerHttpService } from './Services/server-http.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'SalesSystemDemo';
+  @ViewChild('sidenav') sidenav: MatSidenav | any;
+  public isOpened = false;
+  public totalStudents = 0;
+
+  constructor(
+    private common: CommonService,
+    private serverHttp: ServerHttpService
+  ) {}
+  
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  public openLeftSide() {
+    this.isOpened = !this.isOpened;
+    this.sidenav.toggle();
+  }
+
+  public closeLeftSide() {
+    this.isOpened = false;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,15 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
+import { MatSliderModule } from '@angular/material/slider';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatInputModule } from '@angular/material/input';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -11,6 +20,7 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
 import { BoldDirective } from './Directives/bold.directive';
 import { FormComponent } from './form/form.component';
 import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -25,9 +35,18 @@ import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
   imports: [
     BrowserModule,
     HttpClientModule,
+    MatSliderModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatBadgeModule,
+    MatInputModule,
+    MatSidenavModule,
+    MatListModule,
+    MatButtonModule,
     FormsModule,
     ReactiveFormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
